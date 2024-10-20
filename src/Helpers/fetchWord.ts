b640fd2a95d44748247e21f58f060e8c84c68e84
aungchanpyae <khantlu24@gmail.com>
2024-10-20 20:56:11 +0630

    remove test array

--- src/Helpers/fetchWord.ts
+++ src/Helpers/fetchWord.ts
@@ -1,8 +1,7 @@
 export const fetchWord = async () => {
-  // const url = "https://words.dev-apis.com/word-of-the-day";
-  // const fetchUrl = await fetch(url);
-  // const JsonData = await fetchUrl.json();
-  // const array = JsonData.word.toUpperCase().split("");
-  const array = ["H", "E", "L", "L", "O"];
+  const url = "https://words.dev-apis.com/word-of-the-day";
+  const fetchUrl = await fetch(url);
+  const JsonData = await fetchUrl.json();
+  const array = JsonData.word.toUpperCase().split("");
   return array;
 };
